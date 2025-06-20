f0b2e2709daae60673b1bafac92cced44982291f
Shubham Kumar <insshubh22@gmail.com>
2025-06-20 08:55:20 +0530

    uploaded
    Initial File

---
+++ GuessGame/src/Main/permutation.java
@@ -0,0 +1,10 @@
+package Main;
+
+import java.util.Random;
+
+public class permutation {
+	public static void main(String[] args) {
+		Random rd = new Random();
+		System.out.println(rd.nextInt(10));
+	}
+}
